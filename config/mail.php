d2d4fc912c79183d1bafb3414d30af769c7cc0a4
Aleksandr Ivanovitch <app@mwspace.com>
2019-09-23 02:17:35 +0200

    fix mail smtp.google

--- config/mail.php
+++ config/mail.php
@@ -101,6 +101,14 @@ return [
 
     'sendmail' => '/usr/sbin/sendmail -bs',
 
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings

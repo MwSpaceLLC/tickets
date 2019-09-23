027ea670beb93081ba4ab4f0deec77585ec55688
Aleksandr Ivanovitch <app@mwspace.com>
2019-09-23 02:20:11 +0200

    -_-

--- config/mail.php
+++ config/mail.php
@@ -101,14 +101,6 @@ return [
 
     'sendmail' => '/usr/sbin/sendmail -bs',
 
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
